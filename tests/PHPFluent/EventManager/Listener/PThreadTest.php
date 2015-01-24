cb8bccc7a4b74b1cdec1ba583f760859076781f7
Felipe Francisco <felipe.cwb@hotmail.com>
2015-01-24 05:48:13 -0200

    Added PThread listener.

---
+++ tests/PHPFluent/EventManager/Listener/PThreadTest.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace PHPFluent\EventManager\Listener;
+
+use PHPFluent\EventManager\Event;
+
+/**
+* @covers PHPFluent\EventManager\Listener\PThread
+*/
+class PThreadTest extends \PHPUnit_Framework_TestCase
+{
+    public function setUp()
+    {
+        parent::setUp();
+        if (! extension_loaded('pthreads')) {
+            $this->markTestSkipped('The pthreads extension is not available.');
+        }
+    }
+
+    public function testConstructorShouldDefineThread()
+    {
+        $thread   = $this->getMock('\Thread');
+        $listener = new PThread($thread);
+
+        $this->assertAttributeEquals($thread, 'thread', $listener);
+    }
+
+    public function testGetThread()
+    {
+        $thread   = $this->getMock('\Thread');
+        $listener = new PThread($thread);
+
+        $this->assertSame($thread, $listener->getThread());
+    }
+
+    public function testExecuteShouldReturnThread()
+    {
+        $thread = $this->getMock('\Thread', array('start'));
+        $thread->expects($this->once())
+               ->method('start');
+
+        $listener = new PThread($thread);
+        $this->assertInstanceOf('\Thread', $listener->execute(new Event('')));
+    }
+}
+
+if (extension_loaded('pthreads')) {
+
+    class WrapThread extends \Thread
+    {
+        public function run()
+        {
+            //doing something...
+        }
+    }
+}
