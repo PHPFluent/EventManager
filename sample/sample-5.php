820ce3131a2cd209277b2763986d75050cfce9ad
Felipe Francisco <felipe.cwb@hotmail.com>
2015-01-27 03:28:58 -0200

    Change the samples, write the comments

--- sample/sample-5.php
+++ sample/sample-5.php
@@ -5,15 +5,21 @@ require_once __DIR__ . '/../vendor/autoload.php';
 use PHPFluent\EventManager\Manager;
 use PHPFluent\EventManager\Listener\Process;
 
-$manager = new Manager();
-$manager->process = new Process(function () {
+// Create a process to be executed in background.
+// Parameter can be callable or Arara\Process\Action\Action instance.
+$process = new Process(function () {
     echo "-process.start: " . date('H:i:s') . PHP_EOL;
     sleep(5);
     echo "-process...end: " . date('H:i:s') . PHP_EOL;
 });
 
-$manager->process();
+$manager = new Manager();
+$manager->addEventListener('process', $process);
 
+// dispatch a non-blocking execution
+$manager->dispatchEvent('process');
+
+// continue executing
 echo '* Script Execution Continued...' . PHP_EOL;
 sleep(1.5);
 echo '* Script Execution Finished!' . PHP_EOL;
--- sample/sample-6.php
+++ sample/sample-6.php
@@ -1,12 +1,25 @@
 <?php
 
+if (! extension_loaded('pthreads')) {
+    echo "!! Need the pthreads extension !!" . PHP_EOL;
+    exit(1);
+}
+
 require_once __DIR__ . '/../vendor/autoload.php';
 
 use PHPFluent\EventManager\Manager;
 use PHPFluent\EventManager\Listener\PThread;
 
+/**
+ * Personalized Thread class
+ * @see http://php.net/pthreads
+ * @see http://php.net/class.thread
+ */
 class MyThread extends \Thread
 {
+    /**
+     * the script process to run in background
+     */
     public function run()
     {
         echo "-thread.start: " . date('H:i:s') . PHP_EOL;
@@ -15,11 +28,17 @@ class MyThread extends \Thread
     }
 }
 
+// create the listener
+$listener = new PThread(
+    new MyThread() // with MyThread
+);
+
 $manager = new Manager();
-$manager->thread = new PThread(new MyThread());
+$manager->addEventListener('thread', $listener);
+// dispatch listener in background.
+$manager->dispatchEvent('thread');
 
-$manager->thread();
-
+//continue executing
 echo '* Script Execution Continued...' . PHP_EOL;
 //sleep(2);
 echo '* Script Execution Finished!' . PHP_EOL;
